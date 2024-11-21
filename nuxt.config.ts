1165ce73016d63d99dd6a61e6ec40a4e8c2144b1
Andrea Lunghi <andrea.lunghi3@gmail.com>
2024-11-21 23:36:45 +0100

    feat(setup): app nuxt modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/i18n',
+    '@vueuse/nuxt',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/content',
+    '@nuxtjs/sitemap',
+    '@nuxt/icon',
+    '@nuxtjs/seo',
+    '@nuxtjs/color-mode',
+    '@primevue/nuxt-module'
+  ]
 })
