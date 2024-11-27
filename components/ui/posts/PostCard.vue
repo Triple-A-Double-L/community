9558554e1212709feb1ab6329f0b690a5bfd062c
Andrea Lunghi <andrea.lunghi3@gmail.com>
2024-11-27 20:25:51 +0100

    feat(posts): add Posts page

---
+++ components/ui/posts/PostCard.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import type { BlogPost } from "~/types";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "../card";
+import { Badge } from "../badge";
+
+interface Props {
+  data: BlogPost;
+}
+
+const { data: post } = defineProps<Props>();
+</script>
+
+<template>
+  <NuxtLink :to="post._path">
+    <Card class="bg-transparent group flex lg:flex-col">
+      <CardHeader>
+        <div
+          class="ring-1 ring-gray-200 dark:ring-gray-800 overflow-hidden aspect-[16/9] rounded-lg"
+        >
+          <NuxtImg
+            :src="post.image?.src"
+            :alt="post.image?.alt"
+            class="object-cover object-top w-full h-full transform transition-transform duration-200 group-hover:scale-105"
+          />
+        </div>
+      </CardHeader>
+      <CardContent>
+        <div class="mb-3">
+          <Badge>{{ post.badge.label }}</Badge>
+        </div>
+        <CardTitle
+          class="text-gray-900 dark:text-white text-xl font-semibold truncate group-hover:text-gray-600 dark:group-hover:text-gray-300 transition-colors duration-200"
+          >{{ post.title }}</CardTitle
+        >
+        <CardDescription
+          class="text-base text-gray-500 dark:text-gray-400 mt-1 line-clamp-2"
+          >{{ post.description }}</CardDescription
+        >
+        <p class="text-sm text-gray-500 dark:text-gray-400 font-medium">
+          {{
+            new Date(post?.date).toLocaleDateString(undefined, {
+              year: "numeric",
+              month: "short",
+              day: "numeric",
+            })
+          }}
+        </p>
+      </CardContent>
+    </Card>
+  </NuxtLink>
+</template>
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,5 +1,39 @@
+<script setup lang="ts">
+import { PageSection } from "~/components/layout";
+import PostCard from "~/components/ui/posts/PostCard.vue";
+import type { BlogPost } from "~/types";
+
+const route = useRoute();
+
+const { data: posts } = await useAsyncData(route.path, () =>
+  queryContent<BlogPost>().find()
+);
+
+console.log(posts.value);
+</script>
+
 <template>
-  <div>
-    asfasdf
-  </div>
+  <PageSection>
+    <div
+      class="relative border-b border-gray-200 dark:border-gray-800 py-12 lg:py-16"
+    >
+      <h1
+        class="text-3xl sm:text-4xl font-bold text-gray-900 dark:text-white tracking-tight"
+      >
+        Posts
+      </h1>
+      <p class="mt-4 text-lg text-gray-500 dark:text-gray-400">
+        Collezioni di consegne
+      </p>
+    </div>
+    <div
+      class="mt-8 pb-24 flex flex-col lg:grid lg:grid-cols-3 gap-x-8 gap-y-16"
+    >
+      <PostCard
+        v-for="post in posts"
+        :key="post.slug"
+        :data="post"
+      />
+    </div>
+  </PageSection>
 </template>
