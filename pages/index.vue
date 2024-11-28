cfd8b3d74c636a7a518468af998c3b1f06b60780
Andrea Lunghi <andrea.lunghi3@gmail.com>
2024-11-28 11:07:04 +0100

    feat(home): add iframe to figma (wip)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,33 @@
+<script setup lang="ts">
+import { PageSection } from "~/components/layout";
+import AspectRatio from "~/components/ui/aspect-ratio/AspectRatio.vue";
+</script>
+
 <template>
   <div>
-    <h1 class="text-red-500">Welcome to the homepage</h1>
+    <PageSection class="py-24 sm:py-32 md:py-40 text-center">
+      <h1
+        class="text-5xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-7xl"
+      >
+        Segnala, proponi e trasforma con un click.
+      </h1>
+      <p class="mt-6 text-lg tracking-tight text-gray-600 dark:text-gray-300">
+        L'applicazione che semplifica
+      </p>
+    </PageSection>
+    <PageSection>
+      <AspectRatio :ratio="16/9" class="bg-gray-900/5 dark:bg-white/5 ring-1 ring-inset ring-gray-900/10 dark:ring-white/10 rounded-xl lg:-m-4 p-4">
+        <iframe
+          src="https://embed.figma.com/proto/nrPSsILSYjesyc5UHjYYa4?node-id=5-3&starting-point-node-id=5%3A3&embed-host=figma-embed-docs"
+          allowfullscreen
+          class="w-full h-full"
+        />
+      </AspectRatio>
+      <iframe
+        src=""
+        allowfullscreen
+        class="w-full h-full"
+      />
+    </PageSection>
   </div>
 </template>
