335e25c93bf8ad0de11ec408d80e05b46b4f5496
Andrea Lunghi <andrea.lunghi3@gmail.com>
2024-12-04 21:45:35 +0100

    feat: add mobile menu

--- components/layout/MainNavbar.vue
+++ components/layout/MainNavbar.vue
@@ -2,6 +2,15 @@
 import { GITHUB_REPO_LINK } from "~/data/const";
 import { Button } from "../ui/button";
 import ThemeSelector from "../ui/common/ThemeSelector.vue";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
 
 const colorMode = useColorMode();
 </script>
@@ -28,17 +37,13 @@ const colorMode = useColorMode();
         />
         Community
       </NuxtLink>
-      <div class="flex items-center gap-4">
+      <div class="hidden lg:flex items-center gap-4">
         <NuxtLink to="/">Home</NuxtLink>
         <NuxtLink to="/about">About</NuxtLink>
         <NuxtLink to="/posts">Posts</NuxtLink>
         <ThemeSelector />
         <Button as-child>
-          <NuxtLink
-            :to="GITHUB_REPO_LINK"
-            target="_blank"
-            rel="noopener"
-          >
+          <NuxtLink :to="GITHUB_REPO_LINK" target="_blank" rel="noopener">
             <Icon
               name="mdi:github"
               class="h-6 w-6 fill-gray-200 dark:fill-gray-800"
@@ -46,6 +51,37 @@ const colorMode = useColorMode();
             Github
           </NuxtLink>
         </Button>
+      </div>
+      <div class="block lg:hidden">
+        <ThemeSelector />
+        <Sheet>
+          <SheetTrigger>
+            <Button size="icon" variant="ghost">
+              <Icon
+                name="mdi:menu"
+                class="h-6 w-6 fill-gray-200 dark:fill-gray-800"
+              />
+            </Button>
+          </SheetTrigger>
+          <SheetContent class="w-full !max-w-none flex flex-col justify-between">
+            <SheetHeader>
+              <NuxtLink to="/">Home</NuxtLink>
+              <NuxtLink to="/about">About</NuxtLink>
+              <NuxtLink to="/posts">Posts</NuxtLink>
+            </SheetHeader>
+            <SheetFooter>
+              <Button class="w-full" as-child>
+                <NuxtLink :to="GITHUB_REPO_LINK" target="_blank" rel="noopener">
+                  <Icon
+                    name="mdi:github"
+                    class="h-6 w-6 fill-gray-200 dark:fill-gray-800"
+                  />
+                  Github
+                </NuxtLink>
+              </Button>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
       </div>
     </div>
   </header>
