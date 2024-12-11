d04cef7481650a2bb1974b8577f81aaabcf3084e
Andrea Lunghi <andrea.lunghi3@gmail.com>
2024-12-11 10:07:51 +0100

    fix: favicon path

--- app.vue
+++ app.vue
@@ -1,20 +1,3 @@
-<script setup lang="ts">
-const setFavicon = () => {
-  const isDark = usePreferredDark();
-  const favicon = computed(() =>
-    isDark.value ? "/img/favicon-dark.ico" : "/img/favicon-light.ico"
-  );
-
-  useFavicon(favicon, {
-    rel: "icon",
-  });
-};
-
-onMounted(() => {
-  setFavicon();
-});
-</script>
-
 <template>
   <Body
     class="antialiased duration-300 transition-colors text-gray-800 dark:text-gray-200 bg-white dark:bg-gray-900 min-h-screen0"
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,20 @@
 <script setup lang="ts">
 import { MainFooter, MainNavbar } from '~/components/layout';
+
+const setFavicon = () => {
+  const isDark = usePreferredDark();
+  const favicon = computed(() =>
+    isDark.value ? "/community/img/favicon-dark.ico" : "/community/img/favicon-light.ico"
+  );
+
+  useFavicon(favicon, {
+    rel: "icon",
+  });
+};
+
+onMounted(() => {
+  setFavicon();
+});
 </script>
 
 <template>
